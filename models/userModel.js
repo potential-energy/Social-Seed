0ca251063ff460b6e339d7cf1677aea197180c7a
Burak Bozdağ <bozdagburak2552@gmail.com>
2022-07-16 20:43:11 +0300

    Kullanıcı tipi sözlüğü oluşturuldu. Kullanıcı tipini döndürebilen fonkiyon yazıldı.

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,66 @@
 const mongoose = require('mongoose')
 const experienceModel = require('./experienceModel')
 
+const userTypes = Object.freeze({
+    "Entrepreneur": { // Default
+        "Newbie": 0,
+        "Pre_Seed": 1,
+        "Pre_Revenue": 2,
+        "Post_Revenue": 3
+    },
+    "Investor": {
+        "Angel": 4,
+        "VC": 5
+    },
+    "NGO": 6,
+    "Ecosystem_Supporter": {
+        "Expert": {
+            "Sector_Expert": 7,
+            "Business_Maker": 8,
+            "HR_Consultant": 9,
+            "Legal_Advisor": 10,
+            "Finance": 11,
+            "Marketing": 12
+        },
+        "Accelerator": 13
+    },
+    "Corporate": {
+        "SME": 14,
+        "Financial_Enterprise": 15,
+        "Production_Company": 16,
+        "Service_Company": 17
+    },
+    "University": {
+        "University_Student_Club": 18,
+        "Technopark": 19,
+        "Technology_Transfer_Office": 20,
+        "Instructor": 21,
+        "University_Entrepreneur": 22,
+        //"Entrepreneurship_Course": 23
+    }
+});
+
+function getUserType(typeNo) {
+    for (var key in userTypes) {
+        if (userTypes[key] == typeNo) {
+            return [key];
+        }
+        else {
+            for (var subkey in userTypes[key]) {
+                if (userTypes[key][subkey] == typeNo) {
+                    return [key, subkey];
+                }
+                else {
+                    for (var subsubkey in userTypes[key][subkey]) {
+                        if (userTypes[key][subkey][subsubkey] == typeNo) {
+                            return [key, subkey, subsubkey];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
 const userSchema = new mongoose.Schema({
     fullname: {
@@ -45,7 +105,8 @@ const userSchema = new mongoose.Schema({
     saved: [{ type: mongoose.Types.ObjectId, ref: 'user' }],
 
     organizations: [{ type: mongoose.Types.ObjectId, ref: 'organization' }],
-    experiences: [{ type: String, type: String}]
+    experiences: [{ type: String, type: String }],
+    userType: { type: Number, default: userTypes.Entrepreneur.Newbie }
 }, {
     timestamps: true
 })
